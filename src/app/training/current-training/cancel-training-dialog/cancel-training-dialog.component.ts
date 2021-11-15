33d5aa62718d99ef1be2ac75e69ecd7938162c77
Amit Dubey <amitdubey1826.com>
2021-11-15 13:33:24 +0530

    Added spinner and dialog

---
+++ src/app/training/current-training/cancel-training-dialog/cancel-training-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel-training-dialog',
+  templateUrl: './cancel-training-dialog.component.html',
+  styleUrls: ['./cancel-training-dialog.component.css']
+})
+export class CancelTrainingDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
