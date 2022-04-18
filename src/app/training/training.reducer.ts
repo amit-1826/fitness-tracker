3d3e3e38f3eeab435c63e56c8dbad556b9371a17
Amit Dubey <amitdubey1826.com>
2022-04-18 22:12:16 +0530

    Finished training actions and reducer

--- src/app/training/training.reducer.ts
+++ src/app/training/training.reducer.ts
@@ -1,11 +1,50 @@
-import { Action } from "@ngrx/store"
+import { ExerciseModel } from "./exercise.model"
+import * as fromApp from '../app.reducer'
+import { SET_AVAILABLE_TRAININGS, SET_FINISHED_TRAININGS, START_TRAINING, STOP_TRAINING, TrainingActions } from "./training.actions"
 
-export interface AuthState {
-    isAuthenticated: boolean
+export interface TrainingState {
+    availableExercises: ExerciseModel[],
+    finishedExercises: ExerciseModel[],
+    activeTraining: ExerciseModel | null
 }
 
-const initialState: AuthState = {
-    isAuthenticated: false
+export interface State extends fromApp.AppState {
+    training: TrainingState
 }
 
-export const getIsAuthenticated = (state: AuthState) => state.isAuthenticated
+const initialState: TrainingState = {
+    availableExercises: [],
+    finishedExercises: [],
+    activeTraining: null
+}
+
+export function trainingReducer(state: TrainingState, actions: TrainingActions) {
+    switch (actions.type) {
+        case SET_AVAILABLE_TRAININGS:
+            return {
+                ...state,
+                availableExercises: actions.payload
+            }
+        case SET_FINISHED_TRAININGS: 
+            return {
+                ...state,
+                finishedExercises: actions.payload
+            }
+        case START_TRAINING:
+            return {
+                ...state,
+                activeTraining: actions.payload
+            }
+        case STOP_TRAINING: 
+            return {
+                ...state,
+                activeTraining: null
+            }
+        default:
+            return state;
+    }
+}
+
+export const getAvailableTrainings = (state: TrainingState) => state.availableExercises;
+export const getFinishedTrainings = (state: TrainingState) => state.finishedExercises;
+export const getCurrentTraining = (state: TrainingState) => state.activeTraining;
